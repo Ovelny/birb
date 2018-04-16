d1c080292ee0c1db67ed99352589bc07159c70b1
Ovelny <this.is.ovelny@gmail.com>
2018-04-16 03:53:10 +0200

    delete last tweet and resend

--- birb.py
+++ birb.py
@@ -2,6 +2,42 @@ from TwitterAPI import TwitterAPI
 from secrets import api
 import click
 
+
+birb = click.Group()
+
+@birb.command('tweet', short_help='send a tweet')
+def tweet():
+    tweet_length = 280
+    tweet = input(colors.PROCESSING + 'Compose your tweet: ' + colors.ENDC)
+    if parse_tweet(tweet, tweet_length):
+        r = api.request('statuses/update', {'status': tweet})
+        print(colors.OKGREEN + 'Tweet sent! (°∋°)' + colors.ENDC 
+              if r.status_code == 200 
+              else colors.FAIL + 'Error: ' + r.text + colors.ENDC)
+    else:
+        print(colors.WARNING +
+              'Your tweet is ' + str(len(tweet) - tweet_length) +
+                 ' character(s) too long, please try again' +
+              colors.ENDC)
+        tweet()
+
+@birb.command('oops', short_help='delete your most recent tweet')
+@click.option('--resend', is_flag=True, help="resend a tweet immediately")
+@click.pass_context
+def delete_last_tweet(context, resend):
+    tweet_id = get_last_tweet_id()
+    r = api.request('statuses/destroy/:' + str(tweet_id))
+    print(colors.FAIL + 'Last tweet deleted' + colors.ENDC
+          if r.status_code == 200 
+          else colors.FAIL + 'Error: ' + r.text + colors.ENDC)
+    if resend:
+        context.invoke(tweet)
+
+
+# ------------
+#  Helpers
+# ------------
+
 # handle colors for terminal
 class colors:
     PROCESSING = '\033[95m' + '[~] '
@@ -11,27 +47,8 @@ class colors:
     FAIL = '\033[91m' + '[-] '
     ENDC = '\033[0m'
 
-
-def print_help():
-    pass
-
 def parse_tweet(tweet, tweet_length):
     return False if len(tweet) > tweet_length else True
-
-def birb():
-    tweet_length = 280
-    tweet = input(colors.PROCESSING + 'Compose your tweet: ' + colors.ENDC)
-    if parse_tweet(tweet, tweet_length):
-        r = api.request('statuses/update', {'status': tweet})
-        print(colors.OKGREEN + 'Tweet sent!' + colors.ENDC 
-              if r.status_code == 200 
-              else colors.FAIL + 'Error: ' + r.text + colors.ENDC)
-    else:
-        print(colors.WARNING +
-              'Your tweet is ', str(len(tweet) - tweet_length),
-                 ' character(s) too long, please try again' +
-              colors.ENDC)
-        birb()
 
 def get_last_tweet_id():
     r = api.request('statuses/user_timeline', {'count': 1})
@@ -46,16 +63,6 @@ def get_last_tweet_id():
               'Stopping: tweet id not found. Status code returned: ' + 
               str(r.status_code) + colors.ENDC)
 
-# optionally repost another tweet if supplied
-def delete_last_tweet():
-    tweet_id = get_last_tweet_id()
-    r = api.request('statuses/destroy/:' + str(tweet_id))
-    print(colors.FAIL + 'Last tweet deleted' + colors.ENDC
-          if r.status_code == 200 
-          else colors.FAIL + 'Error: ' + r.text + colors.ENDC)
 
-# send a tweet with an image
-def image():
-    pass
-
-get_last_tweet_id()
+if __name__ == '__main__':
+    birb()
